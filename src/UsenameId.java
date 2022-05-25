164da12e6adebd3dc2c4c6fcea21c17e5a92ef38
Arunvkp <vkparunkumar@gmail.com>
2022-05-25 11:41:36 +0530

    new A project

--- src/UsenameId.java
+++ src/UsenameId.java
@@ -12,6 +12,8 @@ public class UsenameId {
 		textname.sendKeys("arunkumar@gmail.com");
 		WebElement textpass = driver.findElement(By.name("pass"));
 		textpass.sendKeys("98398759486");
+		WebElement login = driver.findElement(By.id("login"));
+		login.click();
 		
 		
 		
