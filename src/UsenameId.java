b77a725347bc6b0cfb7392e691b67043047d3826
Arunvkp <vkparunkumar@gmail.com>
2022-05-25 10:16:45 +0530

    message

---
+++ src/UsenameId.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsenameId {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nammalu.com\\eclipse-workspace\\SeleniumLocators\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		WebElement textname = driver.findElement(By.id("email"));
+		textname.sendKeys("arunkumar@gmail.com");
+		WebElement textpass = driver.findElement(By.name("pass"));
+		textpass.sendKeys("98398759486");
+		
+		
+		
+		
+	}
+
+}
